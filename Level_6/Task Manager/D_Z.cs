de62b3610ea5f8b1716c6d58d9fac29ad04640b1
SpYDeR <81887114+Spiderkind@users.noreply.github.com>
2021-04-21 17:32:07 +0300

    Create D_Z.cs

---
+++ Level_6/Task Manager/D_Z.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Level_6
+{
+    class HomeWork
+    {
+        static void Main(string[] args)
+        {
+            Task_m pr = new Task_m();
+            pr.GetProcess();
+        }
+    }
+}
