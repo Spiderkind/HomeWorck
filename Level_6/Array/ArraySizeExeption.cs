67c472989c0cdd2f665aa05ab550d05140ddd5e2
SpYDeR <81887114+Spiderkind@users.noreply.github.com>
2021-04-21 17:39:21 +0300

    Create ArraySizeExeption.cs

---
+++ Level_6/Array/ArraySizeExeption.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Level_6
+{
+    [Serializable]
+    class ArraySizeExeption : Exception
+    {
+        public ArraySizeExeption()
+        {
+
+        }
+    }
+}
