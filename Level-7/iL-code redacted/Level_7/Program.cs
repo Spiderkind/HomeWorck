1b4f534ce30dbdd2f9c7fe84bf37b9d05ad70b57
SpYDeR <levashov_vanya@mail.ru>
2021-04-25 19:36:27 +0300

    Add il code

---
+++ Level-7/iL-code redacted/Level_7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Level_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pass = "1234560";
+            Console.WriteLine("ENETR PASSWROD:");
+            string inp = Console.ReadLine();
+            if (inp == pass)
+            {
+                Console.WriteLine("Hello inturder!");
+            }
+        }
+    }
+}
