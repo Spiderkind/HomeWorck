fb83e55b90b456130066da93648f67510e142229
SpYDeR <levashov_vanya@mail.ru>
2021-04-27 19:50:37 +0300

    Level_8 add

---
+++ Level-8/Library.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_Library
+{
+    public class Library
+    {
+       public static  void SayHello()
+        {
+            Console.WriteLine("Hello Friend!");
+        }
+    }
+}
---
+++ Level-8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Hello_Library;
+
+namespace Level_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library.SayHello();
+        }
+    }
+}
